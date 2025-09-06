27ed02423e07e8209495c63de1e0f7956aa3f3a1
jo wonhee <wonhee6985@gmail.com>
2025-09-06 19:55:34 +0900

    c

---
+++ src/B1010.java
@@ -0,0 +1,31 @@
+import java.io.*;
+//1010
+public class B1010 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int C= Integer.parseInt(br.readLine().strip());
+
+        while (C-- > 0) {
+            String[] input = br.readLine().strip().split(" ");
+
+            int N = Integer.parseInt(input[0]);
+            int M = Integer.parseInt(input[1]);
+            int r;
+            long res=1;
+            r= Math.min(N,M-N);
+            for(int i=1; i<=r; i++){
+               res = res*(M - r +i)/i;
+            }
+
+            sb.append(res).append("\n");
+
+        }
+
+        System.out.print(sb);
+
+        br.close();
+
+    }
+}
